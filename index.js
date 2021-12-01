6a55a292adf0e74b113089da1203dd692465ae34
Mirna Pibernik <mpibernik@croz.net>
2021-12-02 00:16:53 +0100

    Add digital signature verification

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const net = require('net');
 const fs = require('fs');
+const crypto = require('crypto');
+
 const port = 12345;
 const host = '127.0.0.1';
 
@@ -13,29 +15,56 @@ let sockets = [];
 server.on('connection', function (sock) {
   console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
   sockets.push(sock);
+  sock.write('CONNECTED: ' + sock.remoteAddress);
 
   sock.on('data', function (data) {
-    if (data == 'sendingPDF') {
-      sock.pipe(fs.createWriteStream('./report.pdf'));
+    if (data.toString() == 'sendingPublicKey') {
+      sock.write('receivingPublicKey');
+      sock.pipe(fs.createWriteStream('./public_key.pem'));
+      sock.write('receivedPublicKey');
     }
-    // console.log('DATA ' + sock.remoteAddress + ': ' + data);
-    // Write the data back to all the connected, the client will receive it as data from the server
-    // sockets.forEach(function (sock, index, array) {
-    //   sock.write(
-    //     sock.remoteAddress + ':' + sock.remotePort + ' said ' + buffer + '\n'
-    //   );
-    // });
+    if (data.toString() == 'sendingSignature') {
+      sock.write('receivingSignature');
+      sock.pipe(fs.createWriteStream('./signature.bin'));
+      sock.write('receivedSignature');
+    }
+    if (data.toString() == 'sendingDocument') {
+      sock.write('receivingDocument');
+      sock.pipe(fs.createWriteStream('./document'));
+      // const fileStream = fs.createWriteStream('./document');
+      // fileStream.on('ready', () => sock.pipe(fileStream));
+      sock.write('receivedDocument');
+    }
+    if (data.toString() == 'verifySignature') {
+      const document = fs.readFileSync('./document');
+      const hash = crypto.createHash('sha256');
+      hash.update(document);
+      const publicKey = crypto.createPublicKey(
+        fs.readFileSync('./public_key.pem')
+      );
+      const signature = fs.readFileSync('./signature.bin');
+
+      const result = crypto.publicDecrypt(publicKey, signature);
+      if (result.compare(hash.digest()) == 0) {
+        sock.write('valid');
+        console.log('valid');
+      } else {
+        sock.write('invalid');
+        console.log('invalid');
+      }
+      sock.destroy();
+    }
   });
 
   // Add a 'close' event handler to this instance of socket
   sock.on('close', function (data) {
-    let index = sockets.findIndex(function (o) {
-      return (
-        o.remoteAddress === sock.remoteAddress &&
-        o.remotePort === sock.remotePort
-      );
-    });
-    if (index !== -1) sockets.splice(index, 1);
+    // let index = sockets.findIndex(function (o) {
+    //   return (
+    //     o.remoteAddress === sock.remoteAddress &&
+    //     o.remotePort === sock.remotePort
+    //   );
+    // });
+    // if (index !== -1) sockets.splice(index, 1);
     console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
   });
 });
