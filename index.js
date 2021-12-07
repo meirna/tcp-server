8eb6894f9ff2fd01878057c18e708a48ae67d976
Mirna Pibernik <mpibernik@croz.net>
2021-12-07 01:23:47 +0100

    Transfer multiple files now working

--- index.js
+++ index.js
@@ -6,65 +6,65 @@ const port = 12345;
 const host = '127.0.0.1';
 
 const server = net.createServer();
-server.listen(port, host, () => {
-  console.log('TCP Server is running on port ' + port + '.');
-});
 
 let sockets = [];
 
-server.on('connection', function (sock) {
-  console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
-  sockets.push(sock);
-  sock.write('CONNECTED: ' + sock.remoteAddress);
+server.on('connection', function (socket) {
+  let body = '';
+  sockets.push(socket);
+  console.log('CONNECTED: ' + socket.remoteAddress + ':' + socket.remotePort);
+  socket.write('CONNECTED: ' + socket.remoteAddress);
 
-  sock.on('data', function (data) {
-    if (data.toString() == 'sendingPublicKey') {
-      sock.write('receivingPublicKey');
-      sock.pipe(fs.createWriteStream('./public_key.pem'));
-      sock.write('receivedPublicKey');
-    }
-    if (data.toString() == 'sendingSignature') {
-      sock.write('receivingSignature');
-      sock.pipe(fs.createWriteStream('./signature.bin'));
-      sock.write('receivedSignature');
-    }
-    if (data.toString() == 'sendingDocument') {
-      sock.write('receivingDocument');
-      sock.pipe(fs.createWriteStream('./document'));
-      // const fileStream = fs.createWriteStream('./document');
-      // fileStream.on('ready', () => sock.pipe(fileStream));
-      sock.write('receivedDocument');
-    }
-    if (data.toString() == 'verifySignature') {
-      const document = fs.readFileSync('./document');
-      const hash = crypto.createHash('sha256');
-      hash.update(document);
-      const publicKey = crypto.createPublicKey(
-        fs.readFileSync('./public_key.pem')
-      );
-      const signature = fs.readFileSync('./signature.bin');
-
-      const result = crypto.publicDecrypt(publicKey, signature);
-      if (result.compare(hash.digest()) == 0) {
-        sock.write('valid');
-        console.log('valid');
-      } else {
-        sock.write('invalid');
-        console.log('invalid');
-      }
-      sock.destroy();
-    }
+  socket.on('data', (data) => {
+    body += data;
   });
 
   // Add a 'close' event handler to this instance of socket
-  sock.on('close', function (data) {
-    // let index = sockets.findIndex(function (o) {
-    //   return (
-    //     o.remoteAddress === sock.remoteAddress &&
-    //     o.remotePort === sock.remotePort
-    //   );
-    // });
-    // if (index !== -1) sockets.splice(index, 1);
-    console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+  socket.on('close', function (data) {
+    const result = [...body.toString().split('\n\n\n\n')];
+
+    const file1bytes = parseInt(result[0]);
+    const file1Name = result[1];
+    const buffer1 = Buffer.alloc(file1bytes, result[2], 'binary');
+    fs.writeFileSync('./' + file1Name, buffer1);
+    const file2bytes = parseInt(result[3]);
+    const file2Name = result[4];
+    const buffer2 = Buffer.alloc(file2bytes, result[5], 'binary');
+    fs.writeFileSync('./' + file2Name, buffer2);
+
+    const hash = crypto.createHash('sha512');
+    const document = fs.readFileSync('./' + file1Name);
+    hash.update(document);
+    const publicKey = crypto.createPublicKey(
+      fs.readFileSync('./public_key.pem')
+    );
+    const signature = fs.readFileSync('./signature.bin');
+    const certificate = fs.readFileSync('./my-certificate.cert');
+    const x509 = new crypto.X509Certificate(certificate);
+
+    const res = crypto.publicDecrypt(publicKey, signature);
+
+    if (res.compare(hash.digest()) == 0 && x509.verify(publicKey)) {
+      socket.write('valid');
+      console.log('valid');
+      // console.log(x509.subject);
+    } else {
+      socket.write('invalid');
+      console.log('invalid');
+      // console.log(x509.subject);
+    }
+
+    let index = sockets.findIndex(function (o) {
+      return (
+        o.remoteAddress === socket.remoteAddress &&
+        o.remotePort === socket.remotePort
+      );
+    });
+    if (index !== -1) sockets.splice(index, 1);
+    console.log('CLOSED: ' + socket.remoteAddress + ' ' + socket.remotePort);
   });
+});
+
+server.listen(port, host, () => {
+  console.log('TCP Server is running on port ' + port + '.');
 });
